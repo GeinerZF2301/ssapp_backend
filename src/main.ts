551a2607f83069fd65e037a92af705a833467ec6
JosephIvan1406Fost <ivanalvarado1406@gmail.com>
2023-11-11 08:25:11 -0600

    no

--- src/main.ts
+++ src/main.ts
@@ -2,30 +2,39 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpException, HttpStatus, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api/v1');
+
+  // Configuración de encabezados CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Cambia esto a la URL de tu aplicación React
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       exceptionFactory: errors => {
         return new HttpException({ message: 'Validation Errors', errors }, HttpStatus.UNPROCESSABLE_ENTITY);
       },
-     whitelist: true,
-     forbidNonWhitelisted: true,
-     transform: true,
-   }),
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
   );
-  
-  const config = new DocumentBuilder()
-  .setTitle('Cats example')
-  .setDescription('The cats API description')
-  .setVersion('1.0')
-  .addTag('cats')
-  .build();
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
 
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(5000);
 }
+
 bootstrap();
